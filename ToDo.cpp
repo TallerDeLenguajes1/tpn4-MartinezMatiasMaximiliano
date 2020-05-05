032808aacebcc22578e3c47319c1a0258f97b0d4
Matias <matumartinez9@hotmail.com>
2020-05-05 02:45:51 -0300

    modificado ToDo.cpp para nuevo enunciado

--- ToDo.cpp
+++ ToDo.cpp
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <string.h>
 
@@ -9,118 +9,95 @@ struct Tarea{
     int Duracion; //91+10;
 };
 
-//funciones
-void CargarTareas(Tarea *tarea,int cantTareas);
-void RealizarTareas(Tarea *Pendiente,Tarea *Realizado,int cantTareas);
-void MostrarPendientes(Tarea *Pendientes);
-void MostrarRealizadas(Tarea *Realizadas);
-
-int main(){
-    srand(time(NULL));
-    int cantTareas = 0;
-    printf("Ingrese la cantidad de tareas a realizar:");
-    scanf("%d",&cantTareas);
-
-    Tarea *TareasPendientes = (Tarea*)malloc((sizeof(Tarea))*cantTareas);
-    CargarTareas(TareasPendientes,cantTareas);
-    Tarea *TareasRealizadas =(Tarea*)malloc((sizeof(Tarea))*cantTareas);
-    RealizarTareas(TareasPendientes,TareasRealizadas,cantTareas);
-    MostrarPendientes(TareasPendientes);
-    MostrarRealizadas(TareasRealizadas);
-    return 0;
-}
-
-void CargarTareas(Tarea *tarea,int cantTareas){
-    Tarea nueva;
+void CargarTarea(Tarea **TareaLocal,int cantTareas){
     for (int i = 0; i < cantTareas; i++)
     {
-        nueva.TareaID = i+1;
-
-        char *Descripcion;
-        int tamanio=0;
-        char descrip[500];
+        Tarea *nueva = (Tarea*)malloc(sizeof(Tarea));
+        nueva->TareaID = i+1;
+        
         printf("Ingrese una descripcion para la tarea [%d]:",i+1);
-            fflush(stdin);
-            gets(descrip);
-            tamanio = strlen(descrip);
-            nueva.Descripcion = (char*)malloc(sizeof(char)*tamanio);
-            strcpy(nueva.Descripcion,descrip);
-
-        nueva.Duracion = rand()%91+10;
-
-        *tarea = nueva;
-        tarea++;
-    }    
-}
-
-void RealizarTareas(Tarea *Pendiente,Tarea *Realizado,int cantTareas){
-    Tarea mostrarTarea;
-    int confirmar;
-    for(int i=0;i < cantTareas;i++){
-        mostrarTarea = *Pendiente;    
-        printf("\n\n=========Tarea [%d]=========\n\n",i+1);
-        printf("ID:%d\n",mostrarTarea.TareaID);
-        printf("Descripcion:%s\n",mostrarTarea.Descripcion);
-        printf("Duracion:%d\n",mostrarTarea.Duracion);
-
-
-        do{
-            printf("Se realizo la tarea?[1/0]");
-            fflush(stdin);
-            scanf("%d",&confirmar);
-        }while(confirmar<0 || confirmar>1);
-        
-        if(confirmar == 1){
-            *Realizado = *Pendiente;
-            Pendiente[i] = NULL;
-        }else{
-            (Realizado->TareaID)--;
-        }
-       Pendiente++;
-       Realizado++;
-    }
-}
-
-void MostrarPendientes(Tarea *Pendientes){
-    printf("\n\n=========TAREAS PENDIENTES=========\n\n");
-    while(Pendientes != NULL){
-        Tarea aux;
-        aux = *Pendientes;
-        printf("ID:%d\n",aux.TareaID);
-        printf("Descipcion:%s\n",aux.Descripcion);
-        printf("Duracion%d\n",aux.Duracion);
-        printf("\n----------------\n");
-        Pendientes++; 
-    }
-}
-
-void MostrarRealizadas(Tarea *Realizadas){
-    printf("\n\n=========TAREAS REALIZADAS=========\n\n");
-    while(Realizadas != NULL){
-        Tarea aux;
-        aux = *Realizadas;
-        printf("ID:%d\n",aux.TareaID);
-        printf("Descipcion:%s\n",aux.Descripcion);
-        printf("Duracion%d\n",aux.Duracion);
-        printf("\n----------------\n");
-        Realizadas++; 
-    }
-}
-
-
-
-/*
-        if(confirmar != 'Y' || confirmar != 'N' || confirmar != 'n' || confirmar != 'y'){
-        printf("Se realizo la tarea?[Y/N]");
         fflush(stdin);
-        confirmar = getchar();
+        char *Descripcion;
+        char descrip[500];
+        gets(descrip);
+        nueva->Descripcion = (char*)malloc(sizeof(char)*strlen(descrip));
+        strcpy(nueva->Descripcion,descrip);
+
+        nueva->Duracion = rand()% 91+10;
+
+        *TareaLocal = nueva;
+
+        *TareaLocal++;
+    }
+    
+}
+void RealizarTarea(Tarea **TareaLocal,Tarea **TareaRLocal,int cantTareas){
+    for (int i = 0; i < cantTareas; i++)
+    {
+        int confirm;
+        printf("TareaID:%d\n",TareaLocal[i]->TareaID);
+        printf("Descripcion:%s\n",TareaLocal[i]->Descripcion);
+        printf("Duracion:%d\n",TareaLocal[i]->Duracion);
+        printf("\n");
+
+        printf("Se realizo la tarea?[1/0]");
+        scanf("%d", &confirm);
+        
+        while(confirm < 0 || confirm > 1){
+            printf("Se realizo la tarea?[1/0]");
+            scanf("%d", &confirm);
+            printf("\n");
+        }
+
+        if(confirm == 1){
+            TareaRLocal[i] = TareaLocal[i];
+            TareaLocal[i] = NULL;
+        }
+
+        if (confirm == 0)
+        {
+            TareaRLocal[i] == NULL;
         }
         
-        if(confirmar == 'Y'){
-            *Realizado = mostrarTarea;
-            Realizado++; 
-            Pendiente++;
-        }else{
-            Pendiente++;
+    }
+    
+}
+
+void MostrarLista(Tarea **Lista,int cantTareas){
+    for (int i = 0; i < cantTareas; i++)
+    {
+        if(Lista[i] != NULL){
+        printf("TareaID:%d\n",Lista[i]->TareaID);
+        printf("Descripcion:%s\n",Lista[i]->Descripcion);
+        printf("Duracion:%d\n",Lista[i]->Duracion);
+        printf("\n"); 
         }
-        */
+    }
+    
+}
+
+
+
+int main(){
+    int cantTareas = 3;
+    /*
+    printf("Ingrese la cantidad de tareas:");
+    scanf("%d",&cantTareas);
+    */
+
+   Tarea **TareasPendientes = (Tarea**)malloc(sizeof(Tarea)*cantTareas);
+   Tarea **TareasRealizadas = (Tarea**)malloc(sizeof(Tarea)*cantTareas);
+
+    CargarTarea(TareasPendientes,cantTareas);
+    printf("\n\n=============LISTA DE TAREAS=============\n\n");
+    RealizarTarea(TareasPendientes,TareasRealizadas,cantTareas);
+
+    printf("\n\n=============LISTA DE TAREAS PENDIENTES=============\n\n");
+    MostrarLista(TareasPendientes,cantTareas);
+    printf("\n\n=============LISTA DE TAREAS REALIZADAS=============\n\n");
+    MostrarLista(TareasRealizadas,cantTareas);
+    
+    
+
+    return 0;
+}
